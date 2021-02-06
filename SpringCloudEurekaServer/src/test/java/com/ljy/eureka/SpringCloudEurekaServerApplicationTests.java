0168b748a8f16ed14f080c9e45a39a5efd0833dc
wodyd202 <wodyd202@naver.com>
2021-02-06 22:57:35 +0900

    - 프로젝트 시작

---
+++ SpringCloudEurekaServer/src/test/java/com/ljy/eureka/SpringCloudEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljy.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
